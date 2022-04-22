42a408f2aa068ab41fedfde9bdd475be2300235b
Mudit Gurjar <mudit182@gmail.com>
2022-04-22 12:15:42 +0530

    - github action: removed step to copy code - github action: added step to clone fresh project and deploy

--- script.sh
+++ script.sh
@@ -15,7 +15,7 @@ then
    # Stop all backend project's containers and build, start backend stack containers and run tests
    docker-compose -f docker-compose.yml -f compose.fastapi.yml -f compose.mysql.yml -f compose.vueapp.yml -p backend down
    docker-compose -f docker-compose.yml -f compose.fastapi.yml -f compose.mysql.yml -p backend --profile backend run fastapi pytest --alluredir='./test/allure-results'
-elif [ $case = "launch-app" ]
+elif [ $case = "launch-app-dev-env" ]
 then
    # Stop all backend project's containers and build and start backend stack containers for production
    echo "Launching backend api & `mysql` container"
