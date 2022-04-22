22096f9a28c056c1f923944731b20209ff97ef31
Mudit Gurjar <mudit182@gmail.com>
2022-04-22 10:59:57 +0530

    - fixed deploy step to pull latest code before launching app - fixed launch script - referencing incorrect compose profile

--- script.sh
+++ script.sh
@@ -2,18 +2,26 @@
 case=${1:-default}
 if [ $case = "launch-app-local" ]
 then
-   docker-compose -f docker-compose.yml -f compose.fastapi.yml -f compose.mysql.yml -p backend --profile backend-serve up --build
+   # Stop all backend project's containers and build and start backend stack containers
+   docker-compose -f docker-compose.yml -f compose.fastapi.yml -f compose.mysql.yml -f compose.vueapp.yml -p backend down
+   docker-compose -f docker-compose.yml -f compose.fastapi.yml -f compose.mysql.yml -p backend --profile backend up --build
 elif [ $case = "tdd" ]
 then
-   docker-compose -f docker-compose.yml -f compose.fastapi.yml -f compose.mysql.yml -p backend --profile backend-test run fastapi ptw -- --testmon
+   # Stop all backend project's containers and build, start backend stack containers and run tests with watch
+   docker-compose -f docker-compose.yml -f compose.fastapi.yml -f compose.mysql.yml -f compose.vueapp.yml -p backend down
+   docker-compose -f docker-compose.yml -f compose.fastapi.yml -f compose.mysql.yml -p backend --profile backend run fastapi ptw -- --testmon
 elif [ $case = "run-api-tests" ]
 then
-   docker-compose -f docker-compose.yml -f compose.fastapi.yml -f compose.mysql.yml -p backend --profile backend-test run fastapi pytest --alluredir='./test/allure-results'
+   # Stop all backend project's containers and build, start backend stack containers and run tests
+   docker-compose -f docker-compose.yml -f compose.fastapi.yml -f compose.mysql.yml -f compose.vueapp.yml -p backend down
+   docker-compose -f docker-compose.yml -f compose.fastapi.yml -f compose.mysql.yml -p backend --profile backend run fastapi pytest --alluredir='./test/allure-results'
 elif [ $case = "launch-app" ]
 then
+   # Stop all backend project's containers and build and start backend stack containers for production
    echo "Launching backend api & `mysql` container"
 #    export DATABASE_URL="todo-for-prod"
-   docker-compose -f docker-compose.yml -f compose.fastapi.yml -f compose.mysql.yml -p backend --profile backend-serve up --build
+   docker-compose -f docker-compose.yml -f compose.fastapi.yml -f compose.mysql.yml -f compose.vueapp.yml -p backend down
+   docker-compose -f docker-compose.yml -f compose.fastapi.yml -f compose.mysql.yml -p backend --profile backend up --build
 else
    echo "no option passed"
    echo "available options are:
