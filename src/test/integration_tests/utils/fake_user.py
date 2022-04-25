6b5ac0f5e912f45b384637f6185a27d8fea69c27
Mudit Gurjar <mudit182@gmail.com>
2022-04-25 21:13:47 +0530

    - minor refactoring

--- src/test/integration_tests/utils/fake_user.py
+++ src/test/integration_tests/utils/fake_user.py
@@ -10,9 +10,10 @@ from data.schemas.users.userCreate import UserCreate
 logger = logging.getLogger(__name__)
 
 fake = Faker()
+person = Person()
 
 def generate_random_user_to_create() -> UserCreate:
-    user_email = Person().email(domains=['fakegmail.com', 'fakeyahoo.com'])
+    user_email = person.email(domains=['fakegmail.com', 'fakeyahoo.com'])
     user_password = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
     user = UserCreate(email=user_email, password=user_password)
     return user
