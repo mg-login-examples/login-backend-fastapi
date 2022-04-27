40f28bb7f341e52d578c7127c39c8b5cd24076ab
Mudit Gurjar <mudit182@gmail.com>
2022-04-27 20:34:48 +0530

    - added negative tests for authentication endpoint

--- src/test/integration_tests/tests/api_endpoints/test_authenticate.py
+++ src/test/integration_tests/tests/api_endpoints/test_authenticate.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 authenticate_url = "api/authenticate/"
 
 def test_authentication_no_access_token(test_client_authenticated: requests.Session):
-    test_client_authenticated.cookies.clear()
+    test_client_authenticated.cookies.pop("Authorization")
     response = test_client_authenticated.post(authenticate_url)
     assert response.status_code == 403
 
@@ -24,16 +24,40 @@ def test_authentication_valid_cookie(created_user: User, login_response: LoginRe
     user = User(**response.json())
     assert user.id == created_user.id
 
-def test_authentication_invalid_cookie(test_client_authenticated: requests.Session):
+def test_authentication_valid_header(created_user: User, login_response: LoginResponse, test_client_authenticated: requests.Session):
+    test_client_authenticated.cookies.pop("Authorization")
+    response = test_client_authenticated.post(
+        authenticate_url,
+        headers={"Authorization": f"Bearer {login_response.access_token}"}
+    )
+    assert response.status_code == 200
+    user = User(**response.json())
+    assert user.id == created_user.id
+
+def test_authentication_invalid_cookie_wrong_access_token(test_client_authenticated: requests.Session):
     test_client_authenticated.cookies.pop("Authorization")
     test_client_authenticated.cookies.set("Authorization", '"Bearer invalid-cookie"')
     response = test_client_authenticated.post(authenticate_url)
     assert response.status_code == 403
 
-def test_authentication_cookie_without_bearer(test_client_authenticated: requests.Session):
-    authorization_cookie = test_client_authenticated.cookies.get("Authorization")
-    access_token = authorization_cookie[1:-1].split("Bearer")[1].strip()
+def test_authentication_invalid_cookie_no_bearer(login_response: LoginResponse, test_client_authenticated: requests.Session):
     test_client_authenticated.cookies.pop("Authorization")
-    test_client_authenticated.cookies.set("Authorization", f'"{access_token}"')
+    test_client_authenticated.cookies.set("Authorization", f'"{login_response.access_token}"')
     response = test_client_authenticated.post(authenticate_url)
+    assert response.status_code == 403
+
+def test_authentication_invalid_header_wrong_access_token(test_client_authenticated: requests.Session):
+    test_client_authenticated.cookies.pop("Authorization")
+    response = test_client_authenticated.post(
+        authenticate_url,
+        headers={"Authorization": f"Bearer invalid-cookie"}
+    )
+    assert response.status_code == 403
+
+def test_authentication_invalid_cookie_no_bearer(login_response: LoginResponse, test_client_authenticated: requests.Session):
+    test_client_authenticated.cookies.pop("Authorization")
+    response = test_client_authenticated.post(
+        authenticate_url,
+        headers={"Authorization": f"{login_response.access_token}"}
+    )
     assert response.status_code == 403
