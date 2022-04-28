44cfe5fdf34c685c337dcc79c6e0834a4233ed59
Mudit Gurjar <mudit182@gmail.com>
2022-04-28 13:31:34 +0530

    - updated pytest script after fixture refactoring

--- script.sh
+++ script.sh
@@ -16,7 +16,7 @@ then
    # Stop all backend project's containers and build, start backend stack containers and run tests
    docker-compose -f docker-compose.yml -f compose.fastapi.yml -f compose.mysql.yml -f compose.vueapp.yml -p backend down
    docker-compose -f docker-compose.yml -f compose.fastapi.yml -f compose.mysql.yml -p backend --profile backend build
-   docker-compose -f docker-compose.yml -f compose.fastapi.yml -f compose.mysql.yml -p backend --profile backend run fastapi pytest --alluredir='./test/allure-results'
+   docker-compose -f docker-compose.yml -f compose.fastapi.yml -f compose.mysql.yml -p backend --profile backend run fastapi python -m pytest --alluredir='./test/allure-results'
 elif [ $case = "launch-app-dev-env" ]
 then
    # Stop all backend project's containers and build and start backend stack containers for production
