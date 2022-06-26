a0f5b69df5d98bff3c102ff55fe26810bda379ca
Mudit Gurjar <mudit182@gmail.com>
2022-06-26 16:23:07 +0530

    - fixed admin app login page and other sub-urls not opening directly when api running as a docker service

--- src/admin/view/mount_admin_app.py
+++ src/admin/view/mount_admin_app.py
@@ -3,7 +3,12 @@ from starlette.staticfiles import StaticFiles, Scope, Response
 
 class SPAStaticFiles(StaticFiles):
     async def get_response(self, path: str, scope: Scope) -> Response:
-        response = await super().get_response(path, scope)
+        # Try/Except needed as it doesn't work inside docker otherwise
+        # 404 Response returned as an exception
+        try:
+            response = await super().get_response(path, scope)
+        except Exception as error_response:
+            response = error_response
         if response.status_code == 404:
             response = await super().get_response('.', scope)
         return response
