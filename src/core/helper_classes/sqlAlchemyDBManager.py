5967c632b3c4b7137761cb0036d285899cfbd3a2
Mudit Gurjar <mudit182@gmail.com>
2022-08-14 00:56:40 +0530

    - added alembic for database migration

--- src/core/helper_classes/sqlAlchemyDBManager.py
+++ src/core/helper_classes/sqlAlchemyDBManager.py
@@ -1,8 +1,9 @@
 import logging
 
-from sqlalchemy import create_engine
-from sqlalchemy.engine import make_url
+from sqlalchemy import create_engine, pool
+from sqlalchemy.engine import make_url, URL
 from sqlalchemy.orm import sessionmaker
+
 
 logger = logging.getLogger(__name__)
 
@@ -11,18 +12,18 @@ class SQLAlchemyDBManager():
     def __init__(self, database_url: str, database_user: str, database_password: str):
         logger.debug("Setting up SQLAlchemy")
 
+        self.sqlalchemy_url = SQLAlchemyDBManager.generate_sqlalchemy_url(
+            database_url,
+            database_user=database_user,
+            database_password=database_password,
+        )
+
         if "sqlite" in database_url:
-            self.URL_CONFIG = make_url(database_url)
             self.engine = create_engine(
-                self.URL_CONFIG, connect_args={"check_same_thread": False}
+                self.sqlalchemy_url, connect_args={"check_same_thread": False}
             )
         else:
-            self.URL_CONFIG = make_url(database_url)
-            if not self.URL_CONFIG.username:
-                self.URL_CONFIG = self.URL_CONFIG.set(username=database_user)
-            if not self.URL_CONFIG.password:
-                self.URL_CONFIG = self.URL_CONFIG.set(password=database_password)
-            self.engine = create_engine(self.URL_CONFIG)
+            self.engine = create_engine(self.sqlalchemy_url)
 
         self.Session = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
 
@@ -32,3 +33,35 @@ class SQLAlchemyDBManager():
             yield session
         finally:
             session.close()
+
+    @staticmethod
+    def generate_sqlalchemy_url(database_url: str, database_user: str, database_password: str) -> URL:
+        sqlalchemy_url = make_url(database_url)
+
+        if not "sqlite" in database_url:
+            if not sqlalchemy_url.username:
+                sqlalchemy_url = sqlalchemy_url.set(username=database_user)
+            if not sqlalchemy_url.password:
+                sqlalchemy_url = sqlalchemy_url.set(password=database_password)
+
+        return sqlalchemy_url
+
+    @staticmethod
+    def create_sqlalchemy_engine_for_alembic(database_url: str, database_user: str, database_password: str):
+        sqlalchemy_url = SQLAlchemyDBManager.generate_sqlalchemy_url(
+            database_url,
+            database_user=database_user,
+            database_password=database_password,
+        )
+
+        if "sqlite" in database_url:
+            return create_engine(
+                sqlalchemy_url, 
+                poolclass=pool.NullPool,
+                connect_args={"check_same_thread": False}
+            )
+        else:
+            return create_engine(
+                sqlalchemy_url, 
+                poolclass=pool.NullPool,
+            )
