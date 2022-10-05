a4b19b02330347196b4838152eea4d2230eff09d
Mudit Gurjar <mudit182@gmail.com>
2022-10-05 23:31:39 +0530

    - added cascade delete for email verifications table - user_id field

--- src/data/database/models/user_email_verification.py
+++ src/data/database/models/user_email_verification.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.sql import func
 
 from data.database.models.base import Base
@@ -11,6 +11,6 @@ class UserEmailVerification(Base):
     time_created = Column(DateTime(timezone=True), server_default=func.now())
     time_updated = Column(DateTime(timezone=True), onupdate=func.now())
     verification_code = Column(Integer, nullable=False)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    user = relationship("User")
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    user = relationship("User", backref=backref("email_verifications", cascade="all,delete"))
     expires_at = Column(DateTime, nullable=False)
