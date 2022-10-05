b211cbf57a79ac81b161644153b11d6af74b1601
Mudit Gurjar <mudit182@gmail.com>
2022-10-05 17:50:45 +0530

    - fixed mysql alembic migration issue due to faulty unique constraint

--- src/data/database/models/user_session.py
+++ src/data/database/models/user_session.py
@@ -7,10 +7,10 @@ from data.database.models.user import User
 class UserSession(Base):
     __tablename__ = "user_sessions"
     __table_args__ = (
-        Index("idx_token", "token", mysql_length=1024),
+        Index("idx_token", "token", mysql_length=50),
     )
     id = Column(Integer, primary_key=True, index=True)
-    token = Column(String(1000), unique=True, index=True)
+    token = Column(String(100), unique=True)
     expires_at = Column(DateTime, nullable=False)
     time_created = Column(DateTime(timezone=True), server_default=func.now())
     time_updated = Column(DateTime(timezone=True), onupdate=func.now())
