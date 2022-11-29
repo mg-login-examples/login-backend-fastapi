6a0b148f13fb826ee3352a88819f9a346388a72f
Mudit Gurjar <mudit182@gmail.com>
2022-11-29 19:14:20 +0530

    - resolve cors error

--- src/core/cors_settings.py
+++ src/core/cors_settings.py
@@ -7,19 +7,14 @@ development_origins = [
     "http://localhost:8019",
     "http://localhost:5173",
     "http://localhost:4173",
+    "http://frontend.login.com:8029",
+    "http://frontend.login.com:8030",
+    "http://full_app_proxy.com",
 ]
 
 cloud_development_origins = [
     "http://frontend.login-example.duckdns.org",
     "https://frontend.login-example.duckdns.org",
-    "http://localhost:8080",
-    "http://localhost:8081",
-    "http://localhost:8019",
-    "http://localhost:5173",
-    "http://localhost:4173",
-    "http://frontend.login.com:8029",
-    "http://frontend.login.com:8030",
-    "http://full_app_proxy.com",
 ]
 
 production_origins = []
