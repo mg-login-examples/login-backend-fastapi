b4137db16d4b94eaf87699311a9a7352407c6fd5
Mudit Gurjar <mudit182@gmail.com>
2022-12-02 00:06:41 +0530

    -

--- src/core/cors_settings.py
+++ src/core/cors_settings.py
@@ -9,7 +9,7 @@ development_origins = [
     "http://localhost:4173",
     "http://frontend.login.com:8029",
     "http://frontend.login.com:8030",
-    "http://frontend.full_app_proxy",
+    "http://frontend.full_app_proxy.com",
 ]
 
 cloud_development_origins = [
