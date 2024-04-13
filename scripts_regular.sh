309eea3c46a1c4f2bd61da38bcee56865022807d
Mudit Gurjar <mudit182@gmail.com>
2024-04-13 22:07:37 +0530

    - fix docker files, scripts

--- scripts_regular.sh
+++ scripts_regular.sh
@@ -1,71 +1,91 @@
-run_db_migrations() { poetry run alembic upgrade head; }
-run_db_migrations_localdb() { poetry run alembic -c alembic.sqlite.ini upgrade head; }
-create_admin_users() { poetry run python main.py add_admin_user $1 $2; }
-
+#!/bin/sh
 case=${1:-default}
-if [ $case = "launch-api-localdb" ]
+if [ $case = "backend" ]
 then
-    cd src
-    # Define test admin users
-    BACKEND_ADMIN_USER_EMAIL="admin@admin.admin"
-    BACKEND_ADMIN_USER_PASSWORD="admin"
-    # run db migrations and create test admin users
-    run_db_migrations_localdb && create_admin_users $BACKEND_ADMIN_USER_EMAIL $BACKEND_ADMIN_USER_PASSWORD
-    poetry run python main.py
-elif [ $case = "launch-api-externaldb" ]
-then
-    cd src
-    # Override localdb env vars
-    export DATABASE_URL="${DATABASE_URL:-mysql://root@mysql_db/login}"
-    export MONGO_HOST="${MONGO_HOST-mongo_db}"
-    export MONGO_USERNAME="${MONGO_USERNAME:-root_user}"
-    export USE_IN_MEMORY_MONGO_DB=False
-    export ACCESS_TOKENS_STORE_TYPE="${ACCESS_TOKENS_STORE_TYPE:-redis}"
-    export REDIS_URL="${REDIS_URL:-redis://redis_cache}"
-    export CHECK_REDIS_CONNECTION_ON_APP_START=True
-    export PUBSUB_URL="${PUBSUB_URL:-redis://redis_cache}"
-    export CORS_ORIGINS_SET="${CORS_ORIGINS_SET:-Development}"
-    export USER_AUTH_COOKIE_TYPE="${USER_AUTH_COOKIE_TYPE:-localhost_development}"
-    export ADMIN_USER_AUTH_COOKIE_TYPE="${ADMIN_USER_AUTH_COOKIE_TYPE:-localhost_development}"
-    # Define test admin users
-    BACKEND_ADMIN_USER_EMAIL="admin@admin.admin"
-    BACKEND_ADMIN_USER_PASSWORD="admin"
-    # run db migrations and create test admin users
-    run_db_migrations && create_admin_users $BACKEND_ADMIN_USER_EMAIL $BACKEND_ADMIN_USER_PASSWORD
-    poetry run python main.py
-elif [ $case = "lint-typecheck" ]
-then
-    cd src
-    poetry run mypy . --exclude 'alembic_sqlite/*' --exclude 'alembic/*' --check-untyped-defs
-elif [ $case = "lint-format" ]
-then
-    cd src
-    poetry run black . --check --verbose --diff --color
-elif [ $case = "run-api-tests" ]
-then
-    cd src
-    # Define test admin users
-    BACKEND_ADMIN_USER_EMAIL="admin@admin.admin"
-    BACKEND_ADMIN_USER_PASSWORD="admin"
-    # run db migrations and create test admin users
-    run_db_migrations_localdb && create_admin_users $BACKEND_ADMIN_USER_EMAIL $BACKEND_ADMIN_USER_PASSWORD
-    poetry run pytest test/integration_and_unit_tests
-elif [ $case = "run-admin-app-tests" ]
-then
-    cd src
-    # Define test admin users
-    BACKEND_ADMIN_USER_EMAIL="admin@admin.admin"
-    BACKEND_ADMIN_USER_PASSWORD="admin"
-    # run db migrations and create test admin users
-    run_db_migrations_localdb && create_admin_users $BACKEND_ADMIN_USER_EMAIL $BACKEND_ADMIN_USER_PASSWORD
-    poetry run pytest test/admin_app_e2e_tests --alluredir='./test/allure-results'
-else
-   echo "no option passed"
-   echo "available options are:
-    - launch-api-local
-    - launch-api-externaldb
-    - lint-backend
-    - run-api-tests
-    - run-admin-app-tests
+  cd src
+  poetry run alembic upgrade head
+  poetry run python main.py add_admin_user admin@admin.admin admin
+  # determine final_command based on user input
+  test_case=${2:-launch}
+  if [ $test_case = "launch" ]
+  then
+    FINAL_COMMAND="python main.py"
+  elif [ $test_case = "api-tests" ]
+  then
+    FINAL_COMMAND="poetry run pytest test/integration_and_unit_tests/ -n 8 --capture=no --alluredir=test/allure-results"
+  elif [ $test_case = "tdd" ]
+  then
+    FINAL_COMMAND="poetry run ptw test/integration_and_unit_tests/ -- -- --testmon"
+  elif [ $test_case = "admin-app-tests" ]
+  then
+    FINAL_COMMAND="poetry run pytest test/admin_app_e2e_tests --alluredir=test/allure-results"
+  elif [ $test_case = "type-check" ]
+  then
+    FINAL_COMMAND="poetry run mypy . --exclude alembic_sqlite/* --exclude alembic/* --check-untyped-defs"
+  elif [ $test_case = "format-check" ]
+  then
+    FINAL_COMMAND="poetry run black . --check --diff --color"
+  elif [ $test_case = "custom" ]
+  then
+    FINAL_COMMAND=${3}
+  else
+    echo "Unknown option passed for backend <option>
+    <option> one of: launch, api-tests, tdd, admin-app-tests, type-check, format-check, custom <your_custom_command>
     "
+    exit 1
+  fi
+  $FINAL_COMMAND
+elif [ $case = "backend-localdb" ]
+then
+  cd src
+  poetry run alembic -c alembic.sqlite.ini upgrade head
+  poetry run python main.py add_admin_user admin@admin.admin admin
+  # determine final_command based on user input
+  test_case=${2:-launch}
+  if [ $test_case = "launch" ]
+  then
+    FINAL_COMMAND="python main.py"
+  elif [ $test_case = "api-tests" ]
+  then
+    FINAL_COMMAND="poetry run pytest test/integration_and_unit_tests/ -n 10 --capture=no --alluredir=test/allure-results"
+  elif [ $test_case = "tdd" ]
+  then
+    FINAL_COMMAND="poetry run ptw test/integration_and_unit_tests/ -- -- --testmon"
+  elif [ $test_case = "admin-app-tests" ]
+  then
+    FINAL_COMMAND="poetry run pytest test/admin_app_e2e_tests --alluredir=test/allure-results"
+  elif [ $test_case = "type-check" ]
+  then
+    FINAL_COMMAND="poetry run mypy . --exclude alembic_sqlite/* --exclude alembic/* --check-untyped-defs"
+  elif [ $test_case = "format-check" ]
+  then
+    FINAL_COMMAND="poetry run black . --check --diff --color"
+  elif [ $test_case = "custom" ]
+  then
+    FINAL_COMMAND=${3}
+  else
+    echo "Unknown option passed for backend-localdb <option>
+    <option> one of: launch, api-tests, tdd, admin-app-tests, type-check, format-check, custom <your_custom_command>
+    "
+    exit 1
+  fi
+  $FINAL_COMMAND
+  else
+    echo "Unknown option passed for frontend <option>
+    <option> one of: build-dev, build-prod, unit-tests, tdd, lint-check, type-check, custom <your_custom_command>
+    "
+    exit 1
+  fi
+  $FINAL_COMMAND
+else
+  echo "no option passed"
+  echo "available options are:
+  - backend <option>
+    <option> one of: launch, api-tests, tdd, admin-app-tests, type-check, format-check, custom <your_custom_command>
+  - backend-localdb <option>
+    <option> one of: launch, api-tests, tdd, admin-app-tests, type-check, format-check, custom <your_custom_command>
+  - frontend <option>
+    <option> one of: build-dev, build-prod, unit-tests, tdd, lint-check, type-check
+  - down
+  "
 fi
