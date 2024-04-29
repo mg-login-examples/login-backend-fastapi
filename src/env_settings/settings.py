fa3891dd8df3f92adb6cc53f1b8e5d7227d52ec6
Mudit Gurjar <mudit182@gmail.com>
2024-04-29 11:41:01 +0530

    - fix port error

--- src/env_settings/settings.py
+++ src/env_settings/settings.py
@@ -4,19 +4,25 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    server_port: int = 8000
+
+    ################################ Server settings ################################
+    server_port: int = 8018
     server_host: str = "0.0.0.0"
+    reload_app_on_change: bool = True
+
+    ################################ Logging settings ################################
     log_level: str = "INFO"
     log_to_file: bool = True
     log_filename: str = "../app.log"
     log_env_vars_on_app_start: bool = False
-    reload_app_on_change: bool = True
 
+    ################################## SQL settings ##################################
     database_url: str = "sqlite:///./sql_app.db"
     database_user: str = ""
     database_password: str = ""
     check_sql_db_connection_on_app_start: bool = True
 
+    ################################# Mongo settings #################################
     mongo_host: str = "localhost"
     mongo_port: int = 27017
     mongo_username: str = ""
@@ -25,17 +31,26 @@ class Settings(BaseSettings):
     use_in_memory_mongo_db: bool = True
     check_mongo_db_connection_on_app_start: bool = True
 
-    access_tokens_store_type: Literal["file", "redis"] = "file"
+    ################################# Redis settings #################################
     redis_url: str = ""
     redis_password: str = ""
     check_redis_connection_on_app_start: bool = False
 
+    ############################## Access Token settings ##############################
+    access_tokens_store_type: Literal["file", "redis"] = "file"
+
+    ############################## Frontend Apps settings ##############################
     add_admin_app: bool = True
     add_password_reset_app: bool = True
 
+    ################################# Cookies settings #################################
     cors_origins_set: Literal[
         "Development", "Cloud-Development", "Production"
     ] = "Development"
+    # cross_site_secure used for production when frontend & backend on different domains,
+    # same_site_secure used for production when frontend & backend on same domain
+    # samesite_not_secure used for docker e2e testing
+    # localhost_development used for local development
     user_auth_cookie_type: Literal[
         "cross_site_secure",
         "same_site_secure",
@@ -48,11 +63,8 @@ class Settings(BaseSettings):
         "same_site_not_secure",
         "localhost_development",
     ] = "localhost_development"
-    # cross_site_secure used for production when frontend & backend on different domains,
-    # same_site_secure used for production when frontend & backend on same domain
-    # samesite_not_secure used for docker e2e testing
-    # localhost_development used for local development
 
+    ############################## Websocket PubSub settings ##############################
     add_websocket: bool = True
     pubsub_url: str = "memory://"
     check_pubsub_connection_on_app_start: bool = True
